1a7cc671ed8327ffe7f95b8f1ae6fdf268b0e713
matjack9 <matjack9@gmail.com>
2018-02-18 19:59:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function logDriverNames(drivers) {
+  for (const person of drivers) {
+    console.log(person.name);
+  }
+}
+
+function logDriversByHometown(drivers, location) {
+  for (const person of drivers) {
+    if (person.hometown === location) {
+      console.log(person.name);
+    }
+  }
+}
+
+function driversByRevenue(drivers) {
+  let current = drivers.slice();
+  return current.sort((driverOne, driverTwo) => driverOne.revenue - driverTwo.revenue);
+}
+
+function driversByName(drivers) {
+  let current = drivers.slice();
+  return current.sort((driverOne, driverTwo) => driverOne.name.localeCompare(driverTwo.name));
+}
+
+function totalRevenue(drivers) {
+  const revenueArr = drivers.map((e) => e.revenue)
+  return revenueArr.reduce((total, amount) => total + amount);
+}
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
