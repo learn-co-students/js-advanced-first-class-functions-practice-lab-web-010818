eff53da5ca99eb7cb1d11c8e8e1b7ba6c398b5b3
krdiamond <github email address>
2018-02-19 13:51:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+// create a variable and set it to a function that can take something in
+// iterate over that something with a .foreach
+// let the .foreach accept another function for each
+// write what the function does
+
+const logDriverNames = function(drivers){
+  drivers.forEach(function(driver){
+    console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function(drivers, hometown){
+  drivers.forEach(function(driver){
+    if (driver.hometown === hometown){
+    console.log(driver.name);
+    }
+  });
+};
+
+//slice method returns a new array
+// the .sort method is destructive so the slice method helps
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.revenue - driverTwo.revenue;
+  });
+};
+
+const driversByName = function(drivers){
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
+
+const totalRevenue = function(drivers){
+  return drivers.reduce(function (total, currentDriver){
+    return currentDriver.revenue + total;
+  },0);
+};
+
+const averageRevenue = function(drivers){
+  return totalRevenue(drivers)/drivers.length;
+};
