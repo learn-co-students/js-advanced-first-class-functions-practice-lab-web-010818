0bfcbd04f1512671b449e24be0de99c9e8509387
Cerunnos <github email address>
2018-02-20 14:53:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const logDriverNames=function(drivers){
+  drivers.forEach(function(driver){
+    return console.log(driver.name)
+  })
+}
+const logDriversByHometown=function(drivers,location){
+  drivers.forEach(function(driver){
+    if (driver.hometown===location){
+      console.log(driver.name)
+    }
+  })
+}
+const driversByRevenue=function(drivers){
+  return drivers.slice().sort(function(driver1, driver2){
+    return driver1.revenue-driver2.revenue
+  })
+}
+const driversByName=function(drivers){
+  return drivers.slice().sort(function(driver1, driver2){
+    return driver1.name.localeCompare(driver2.name)
+  })
+}
+const totalRevenue=function(drivers){
+  return drivers.reduce(function(total, driver){
+    return driver.revenue+total
+  },0)
+}
+const averageRevenue=function(drivers){
+  return totalRevenue(drivers)/drivers.length
+}
