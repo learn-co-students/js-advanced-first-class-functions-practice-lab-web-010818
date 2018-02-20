efcd5425dfbeb9949a86edda6fd15312a6291f12
Alexander Gutterman <alexanderf.gutterman@gmail.com>
2018-02-20 02:44:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  return drivers.map( driver => console.log(driver.name))
+}
+
+function logDriversByHometown(drivers, location) {
+  const filteredDrivers = drivers.filter( driver => driver.hometown === location);
+  return logDriverNames(filteredDrivers);
+}
+
+const driversByRevenue = function(drivers) {
+  return drivers.slice().sort(function(a, b) {
+    return a.revenue - b.revenue
+  })
+}
+
+function driversByName(drivers) {
+  return drivers.slice().sort(function (a, b) {
+    return a.name.localeCompare(b.name)
+  })
+}
+
+const totalRevenue = function(drivers) {
+  return drivers.reduce(function(total, driver) {
+    return total + driver.revenue;
+  }, 0);
+}
+
+const averageRevenue = function(drivers) {
+  return totalRevenue(drivers) / drivers.length
+}
